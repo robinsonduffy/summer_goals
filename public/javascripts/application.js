f34613e48629baa5f79e4c4fc88585356bc15dac
Robinson Duffy <robinson.duffy@gmail.com>
2014-05-18 09:48:17 -0500

    Do it with a click instead of dblclick because iPads are stupid

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$("ul#tasks li.disabled").dblclick(function(){
+	$("ul#tasks li.disabled").click(function(){
 		task_id = $(this).children("input").eq(0).val();
 		window.location.href = window.location.href + "/delete/" + task_id;
 	});
