3c100bf6ff9bbba96df38c01f0c3c242486edd86
Robinson Duffy <rduffy@fatwallet.com>
2017-05-01 22:53:07 -0500

    Changing the error message to say “chores” instead of “tasks”

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -68,12 +68,5 @@ class PagesController < ApplicationController
       redirect_to chore_chart_day_path(params[:kid_id], params[:day_id])
     end
   end
-  
-  def deny_access_to_rewards(kid_id)
-    flash[:error] = "You have not completed all your required tasks today"
-    day_id = Time.current.wday - 1
-    day_id = 6 if day_id == -1
-    redirect_to chore_chart_day_path(kid_id, day_id)
-  end
 
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -5,7 +5,7 @@ module PagesHelper
   end
   
   def deny_access_to_rewards(kid_id)
-    flash[:error] = "You have not completed all your required tasks today"
+    flash[:error] = "You have not completed all your required chores today"
     day_id = Time.current.wday - 1
     day_id = 6 if day_id == -1
     redirect_to chore_chart_day_path(kid_id, day_id)
