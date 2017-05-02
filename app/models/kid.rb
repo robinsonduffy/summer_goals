feeea92bec95e7a57bf4be3c4c9ea7320ed8c8ae
Robinson Duffy <rduffy@fatwallet.com>
2017-05-01 22:03:31 -0500

    method to determine if a kid has completed all the required tasks for today

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -9,4 +9,23 @@ class Kid < ActiveRecord::Base
                     
   validates :points, :numericality => true
   
+  def all_required_tasks_completed_today
+    completed_tasks = []
+    self.completed_tasks.where(:date => Time.current.to_date).each do |completed_task|
+      completed_tasks << Task.find(completed_task.task_id)
+    end
+    self.required_tasks.each do |required_task|
+      return false unless completed_tasks.include? required_task
+    end
+    return true
+  end
+  
+  def required_tasks
+    required_tasks = []
+    self.tasks.each do |task|
+      required_tasks << task if task.required
+    end
+    return required_tasks
+  end
+  
 end
